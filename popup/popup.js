95e37c6f822faaebc6963a421e4d24e1168bbc64
Buntin-Synthia <aokitakumi72@gmail.com>
2022-10-14 16:12:23 +0900

    in university

--- popup/popup.js
+++ popup/popup.js
@@ -1,36 +1,39 @@
+console.log("started");
+console.log(chrome.storage.local.get("discord_token"));
+
+let tokenArea = document.getElementById("token");
+let registButton = document.getElementById("regist");
+let postButton = document.getElementById("postAll");
 
 function registToken() {
-	chrome.storage.local.set({ "token": tokenArea.value }, function () {
-		alert("登録しました");
-	});
+    token = tokenArea.value;
+    chrome.storage.local.set({ discord_token: token }, function () {
+        alert("登録しました" + token);
+        console.log(chrome.storage.local.get("discord_token"));
+        console.log(token);
+    });
+    tokenArea.value = "";
 }
 
 function loadCurrent() {
-	chrome.windows.getCurrent({ populate: true }, function () {});
+    chrome.windows.getCurrent({ populate: true }, function () {});
 }
 
 function postList(ary) {
-    fetch(
-        chrome.storage.local.get("token"), 
-        {
-          method: 'post',
-          headers: {
-            "Content-Type": 'application/json',
-          },
-          body: JSON.stringify({
-            username: 'BuntinChromeAPP',
-            avatar_url:
-              'https://cdn.discordapp.com/attachments/1026145551831543909/1026146054002966628/rusiaAhegao.png',
+    let { token } = chrome.storage.local.get("token");
+    fetch(chrome.storage.local.get("token"), {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            username: "BuntinChromeAPP",
+            avatar_url: "https://cdn.discordapp.com/attachments/1026145551831543909/1026146054002966628/rusiaAhegao.png",
             // contents of the message to be sent
-            content:"おまんこ",
-          }),
-        }
-      );
+            content: "",
+        }),
+    });
 }
 
-const tokenArea = document.getElementById("token");
-const registButton = document.getElementById("regist");
-const postButton = document.getElementById("postAll");
-
 registButton.addEventListener("click", registToken);
-postButton.addEventListener("click", postList([]));
+postButton.addEventListener("click", postList);
