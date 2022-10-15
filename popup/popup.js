04356534c13a12b2e48c52bc8eb9241981a7cae6
Buntin-Synthia <aokitakumi72@gmail.com>
2022-10-16 06:49:31 +0900

    test run finished

--- popup/popup.js
+++ popup/popup.js
@@ -4,13 +4,28 @@ console.log(chrome.storage.local.get("discord_token"));
 let tokenArea = document.getElementById("token");
 let registButton = document.getElementById("regist");
 let postButton = document.getElementById("postAll");
+const token = { test: "test" };
+const message = { username: "ブラウザから", content: "fetch api を利用" };
+
+function postData(url = ``, data = {}) {
+    return fetch(url, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+    }).then((response) => response.text());
+}
+
+/* postData(url, message)
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error)); */
 
 function registToken() {
-    token = tokenArea.value;
+    let token = tokenArea.value;
     chrome.storage.local.set({ discord_token: token }, function () {
-        alert("登録しました" + token);
-        console.log(chrome.storage.local.get("discord_token"));
-        console.log(token);
+        alert("登録しました");
     });
     tokenArea.value = "";
 }
@@ -19,20 +34,8 @@ function loadCurrent() {
     chrome.windows.getCurrent({ populate: true }, function () {});
 }
 
-function postList(ary) {
-    let { token } = chrome.storage.local.get("token");
-    fetch(chrome.storage.local.get("token"), {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            username: "BuntinChromeAPP",
-            avatar_url: "https://cdn.discordapp.com/attachments/1026145551831543909/1026146054002966628/rusiaAhegao.png",
-            // contents of the message to be sent
-            content: "",
-        }),
-    });
+function postList() {
+    console.log(Json.stringify(chrome.storage.local.get("discord_token")));
 }
 
 registButton.addEventListener("click", registToken);
